1b1123df9d3fbd2a4ee0c05afb6272cd21fcabcc
Anders W. Olsen <anders@wiberg.tech>
2017-05-29 13:36:07 +0200

    f

--- src/main.sh
+++ src/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # author: Anders Wiberg Olsen (anders@wiberg.tech)
 
-if [ -z ${$1+x} ]; then
+if [ -z ${1+x} ]; then
   echo "No argument is set..."
 else
   echo "Argument is set: $1"

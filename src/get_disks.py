6a56a788f89278d656ad1dbf63399eedf83dea89
Anders W. Olsen <anders@wiberg.tech>
2017-05-29 15:04:33 +0200

    work later

--- src/get_disks.py
+++ src/get_disks.py
@@ -17,10 +17,14 @@ if not os.path.exists(dstroot):
 count = 0
 for disk in disks.split(","):
     #print "Disk %s = %s" % (count,disk)
-    diskname = os.path.basename(disk)
+    diskpath,diskname = os.path.split(disk)
     filename = "%s%s" % (dstroot,diskname)
     print "Copying %s to %s" % (diskname,dstroot)
-    copyfile(disk,filename)
+
+    # TODO: copy the vm.cfg file to dstroot and replace diskpath with dstroot in that new vm.cfg file.
+    # TODO: Make a script that reads the os simple name from vm.cfg file and make it easier to fetch the right template
+
+    #copyfile(disk,filename)
     #copyfile(disk,diskname)
     #file = open(diskname,"w")
     #file.write(disk)

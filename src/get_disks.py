2304fa733a71edaf5a42c000b2c31fde5f1bb696
Anders W. Olsen <anders@wiberg.tech>
2017-05-29 14:38:53 +0200

    get disk path

--- src/get_disks.py
+++ src/get_disks.py
@@ -6,17 +6,20 @@ import os
 disks = sys.argv[1].replace("\'", "")
 print "argument length: %s" % (len(sys.argv))
 
-if (len(sys.argv)==2):
-    dstroot = "./test-folder"
+dstroot = "./"
+
+if (len(sys.argv) == 2):
+    dstroot = "./test-folder/"
 else:
     dstroot = sys.argv[2]
 
-#os.makedirs(dstroot)
+if not os.path.exists(dstroot):
+    os.makedirs(dstroot)
 
 count = 0
 for disk in disks.split(","):
     print "Disk %s = %s" % (count,disk)
-    diskname = "disk%s" % (count)
+    diskname = "%sdisk%s" % (dstroot,count)
     file = open(diskname,"w")
     file.write(disk)
     file.close

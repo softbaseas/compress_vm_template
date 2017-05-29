132b4051b86366b688884d3c3e28ae2b4644b4a3
Anders W. Olsen <anders@wiberg.tech>
2017-05-29 14:32:11 +0200

    make files

--- src/get_disks.py
+++ src/get_disks.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
 import sys
+import shutil
 
 disks = sys.argv[1].replace("\'", "")
+
+dstroot = "./test-folder"
+
+os.makedirs(dstroot)
 
 count = 0
 for disk in disks.split(","):
     print "Disk %s = %s" % (count,disk)
+    diskname = "disk%s" % (count)
+    file = open(diskname,"w")
+    file.write(disk)
+    file.close
     count += 1;

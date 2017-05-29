bcb79e2404c84f2f74ad5e59c4c3240accb9b907
Anders W. Olsen <anders@wiberg.tech>
2017-05-29 13:30:08 +0200

    menu

--- src/main.sh
+++ src/main.sh
@@ -1,2 +1,14 @@
 #!bin/bash
 # author: Anders Wiberg Olsen (anders@wiberg.tech)
+
+echo "Choose one of the following possibilities:"
+echo " 1 - "
+echo " q - Exit"
+
+while true; do
+  read -p "Choice: " choice
+  case $choice in
+    [1]*) ;;
+    [q]*) exit 0;;
+  esac
+done

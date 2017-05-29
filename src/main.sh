b28672afd227535fb4f05b1a12629a39c5c89388
Anders W. Olsen <anders@wiberg.tech>
2017-05-29 14:07:59 +0200

    list disks in the file

--- src/main.sh
+++ src/main.sh
@@ -8,3 +8,21 @@ if [ -z ${1+x} ]; then
 else
   echo "Argument is set: $vmfile"
 fi
+
+
+
+# grep disk test_vm.cfg : Get the line with all disks
+# sed 's/disk = \[//g'  : Remove "disk = [" from the string
+# sed 's/file://g'      : Remove "file:" from the string
+# sed 's/xv\w\+//g'     : Remove all words starting with "xv"
+# sed 's/,,w!//g'       : Remove ",,w!" from the string
+# sed 's/,,w//g'        : Remove ",,w" from the string
+# sed 's/ //g'          : Remove all spaces in the string
+# sed 's/.$//'          : Remove the last character in the string ("]")
+
+disks_str=grep disk test_vm.cfg | sed 's/disk = \[//g' | sed 's/file://g' | sed 's/xv\w\+//g' | sed 's/,,w!//g' | sed 's/,,w//g' | sed 's/ //g' | sed 's/.$//'
+disks=$(echo $disks | tr "," "\n")
+
+for disk in $disks do
+  echo "> [$disk]"
+done

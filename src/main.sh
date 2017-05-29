58e5e3c061bbb2f8db35b82420a950e55cc8e8e3
Anders W. Olsen <anders@wiberg.tech>
2017-05-29 13:32:09 +0200

    test argument

--- src/main.sh
+++ src/main.sh
@@ -1,14 +1,8 @@
 #!bin/bash
 # author: Anders Wiberg Olsen (anders@wiberg.tech)
 
-echo "Choose one of the following possibilities:"
-echo " 1 - "
-echo " q - Exit"
-
-while true; do
-  read -p "Choice: " choice
-  case $choice in
-    [1]*) ;;
-    [q]*) exit 0;;
-  esac
-done
+if [ -z ${$1+x} ]; then
+  echo "No argument is set..."
+else
+  echo "Argument is set: $1"
+fi
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git pull
-./src/main.sh
+./src/main.sh $1
